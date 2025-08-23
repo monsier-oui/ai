9834fd97d570bfc8d4f0a3ef51d4fc961e789c03
oui <hedgehog.oui@gmail.com>
2025-08-24 01:32:34 +0900

    誕生日含めたアイドルのデータを内部で持つように

--- src/modules/noting/birthday.ts
+++ src/modules/noting/birthday.ts
@@ -1,81 +1,37 @@
-import dayjs, { getCurrentDate } from '@/utils/dayjs.js';
+import dayjs, { getCurrentDate, type Dayjs } from '@/utils/dayjs.js';
+import { Idol,IdolList } from '@/idols';
 
-export const getBirthdayIdol = async (date: string) => {
-  const queryText =
-    encodeURIComponent(`PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX imas: <https://sparql.crssnky.xyz/imasrdf/URIs/imas-schema.ttl#>
-    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-    PREFIX schema: <http://schema.org/>
-    SELECT (sample(?o) as ?date) (sample(?n) as ?name) (sample(?a) as ?age) (sample(?u) as ?IdolListURL)
-    WHERE { 
-      ?sub schema:birthDate ?o; imas:Brand ?b; foaf:age ?a; imas:IdolListURL ?u;
-      rdfs:label ?n;
-      FILTER(regex(str(?o), "${date}" ) && contains(?b, "SideM")).
-    }
-    group by(?n)order by(?name)`);
-  const url = `https://sparql.crssnky.xyz/spql/imas/query?query=${queryText}`;
+export const getBirthdayData = (today: Dayjs) => {
+	const idolList = new IdolList(today);
+	const idols = idolList.getBirthdayIdol(today);
 
-  return await fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(
-          `ステータスコード${response.status}: ${response.statusText}\nURL: ${url}`
-        );
-      }
+	if(idols.length > 0){
+		const next = idolList.getNextBirthdayIdol(today);
+		const nextBirthday = next[0].birthday.date.format('M月D日');
 
-      return response.json();
-    })
-    .then((data) => {
-      const result: {name: string; url: string}[] =
-        data.results.bindings.length > 0
-          ? data.results.bindings.map(
-              ({
-                name,
-                age: _age,
-                IdolListURL,
-              }: {
-                [key: string]: {
-                  type: string;
-                  datatype?: string;
-                  value: string;
-                };
-              }) => {
-                const age = Number(_age.value);
-                const title = age < 20 ? 'くん' : 'さん';
-
-                return {
-                  name: `${name.value}${title}`,
-                  url: IdolListURL.value,
-                };
-              }
-            )
-          : [];
-
-      const names: string[] = [];
-      const urls: string[] = [];
-      for (const idol of result) {
-        names.push(idol.name);
-        urls.push(idol.url);
-      }
-
-      return names.length > 0 ? {
-        name: names.join('、'),
-        url: urls.join(`\n`),
-      } : null;
-    })
-    .catch((error) => {
-      console.error(error);
-
-			return null
-    });
-};
-
-export const createBirthdayNote = async (date: string) => {
-  const data = await getBirthdayIdol(date);
-	if(data){
-		const { name, url } = data
-		return `今日は${name}の誕生日です！ おめでとうございます！` + `${url&&`\n`}${url}`
+		return {
+			idols,
+			next,
+			nextBirthday,
+		};
 	}
 
-	return null
+	return null;
+}
+
+export const createBirthdayNote = (today) => {
+	const birthdayData = getBirthdayData(today);
+	
+	if(birthdayData){
+		const {idols,next,nextBirthday} = birthdayData;
+		const names = idols.map((idol)=>idol.getNameWithTitle()).join('、');
+		const urls = idols.map(({url})=>url).join(`\n`);
+		
+		const nextNames = next.map((idol)=>idol.getNameWithTitle()).join('、');
+
+		return `今日は${names}の誕生日です！ おめでとうございます！\n`
+		+`次のお誕生日は${nextNames}（${nextBirthday}）で、あと${next[0].birthday.daysUntil}日です。\n` + `${urls}`;
+	}
+
+	return null;
 };
